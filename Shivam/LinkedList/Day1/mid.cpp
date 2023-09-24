703f694f1e77e08f555bafb057332e66cb81c3d6
Guptaverse <sggreat88@gmail.com>
2023-09-24 19:27:44 +0530

    Q2 : middle of LL

---
+++ Shivam/LinkedList/Day1/mid.cpp
@@ -0,0 +1,21 @@
+// Find middle of linked list
+
+/*
+
+slow-fast pointer approach
+
+Java : 
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+
+*/
